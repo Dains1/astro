7c87b09d4cb041871dd331414be6ad1a726f60f1
Cybersport <Cybersport@26kadr.internal>
2024-05-28 14:57:29 +0300

    cnopca

--- hhtr/obo3.py
+++ hhtr/obo3.py
@@ -10,13 +10,42 @@ async def get_response2(message_text):
         "modelUri": "gpt://b1gf5pfjsd0phsrq2ldh/yandexgpt-lite",
         "completionOptions": {
             "stream": False,
-            "temperature": 0.4,
+            "temperature": 1,
             "maxTokens": 2000
         },
         "messages": [
             {
                 "role": "system",
-                "text": "Ты -Виртуальный собеседник на любую тему, но кратко, 2-3 предложения"
+                "text": "Ты - рассказчик, рассказываешь истории на любую тему, но не превышающие 30 предложений"
+            },
+            {
+                "role": "user",
+                "text": message_text
+            }
+        ]
+    }
+
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Api-key {TOKENN}"
+    }
+    response = requests.post(url, headers=headers, json=prompt)
+    result = response.json()
+    return result['result']['alternatives'][0]['message']['text']
+
+async def get_response3(message_text):
+    prompt = {
+        "modelUri": "gpt://b1gf5pfjsd0phsrq2ldh/yandexgpt-lite",
+        "completionOptions": {
+            "stream": False,
+            "temperature": 1,
+            "maxTokens": 2000
+        },
+        "messages": [
+            {
+                "role": "system",
+                "text": "Ты - помогаешь людям находить по их предпочтениям игры, аниме и сериалы"
             },
             {
                 "role": "user",
--- main.py
+++ main.py
@@ -1,38 +1,44 @@
 from aiogram import Dispatcher, Bot, types ,executor
 from config import TELEGRAM_TOKEN
 from hhtr.obo1 import generate_image
-from hhtr.obo3 import get_response2
-from hhtr.obo4 import get_response3
+from hhtr.obo3 import get_response2, get_response3
+
 
 bot = Bot(token= TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 
 
 
-@dp.message_handler(commands= 'start')
-async def start(message: types.Message):
-    await message.answer('Привет!')
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button1 = types.KeyboardButton('/рассказчик')
+    button2 = types.KeyboardButton('/помощник')
+    button3 = types.KeyboardButton('/генерация')
+    keyboard.add(button1, button2, button3)
+    await message.reply("Выберите, кем вы хотите, чтобы я был:", reply_markup=keyboard)
 
-@dp.message_handler()
-async def handle_message(message: types.Message):
-    user_text = message.text
+@dp.message_handler(commands=['рассказчик'])
+async def рассказчик_command(message: types.Message):
+    user_text = message.text.replace('/рассказчик ')
+    response_text = await get_response2(user_text)
+    await message.reply(response_text)
+
+@dp.message_handler(commands=['помощник'])
+async def помощник_command(message: types.Message):
+    user_text = message.text.replace('/помощник ')
+    response_text = await get_response3(user_text)
+    await message.reply(response_text)
+
+@dp.message_handler(commands=['генерация'])
+async def генерация_command(message: types.Message):
+    user_text = message.text.replace('/генерация ')
     await message.reply('Идёт генерация изображения')
     try:
         image_data = generate_image(user_text)
         await message.reply_photo(photo=image_data)
     except Exception as e:
         await message.reply(f'Произошла ошибка: {e}')
-
-# @dp.message_handler()
-# async def analize_message(message:types.Message):
-#     response_text2 = await get_response2(message.text)
-#     await message.answer(response_text2)
-#
-# @dp.message_handler()
-# async def analize_message(message:types.Message):
-#     response_text3 = await get_response3(message.text)
-#     await message.answer(response_text3)
-
 
 
 
